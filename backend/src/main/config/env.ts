e64617fc00e58990e7c43462b1b2a527fa49e477
nahan.rezende <nahan.rezende@precato.com.br>
2022-06-07 18:07:20 -0300

    feat: corrigindo .env

--- backend/src/main/config/env.ts
+++ backend/src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:27017/clean-node-api',
-  port: process.env.PORT ?? 3000,
+  port: process.env.PORT ?? 3333,
   jwtSecret: process.env.JWT_SECRET ?? 'xXC0NST@NT1NEXx',
   databaseType: process.env.DATABASE_TYPE ?? 'postgres'
 }
