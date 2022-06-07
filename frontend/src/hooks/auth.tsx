8c36df21a1c866532118709ebefe3cbca81b98fc
Davi Banfi <davibanfi1579@outlook.com>
2022-06-07 15:08:56 -0300

    refactor: change login logic

--- frontend/src/hooks/auth.tsx
+++ frontend/src/hooks/auth.tsx
@@ -1,34 +1,8 @@
 import React, { createContext, useCallback, useContext, useState } from 'react';
-import { decode, JwtPayload } from 'jsonwebtoken';
 import api from '../services/api';
-
-interface IPermission {
-  id: string;
-  name: string;
-  description: string;
-  created_at: string;
-}
-
-interface User {
-  id: string;
-  name: string;
-  email: string;
-  profile: string;
-  profile_id: string;
-  first_access: boolean;
-  ramal: string | undefined;
-  queue: string[];
-  avatar: string | null;
-  avatar_url: string;
-  team_id: string;
-  permissions: IPermission[];
-  loa: string;
-  group: string;
-}
 
 interface AuthState {
   token: string;
-  user: User;
 }
 
 interface SignInCredentials {
@@ -37,10 +11,9 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: User;
+  isLogged: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
-  verifyTokenExpiration(): Promise<void>;
 }
 
 export const AuthContext = createContext<AuthContextData>(
@@ -49,80 +22,44 @@ export const AuthContext = createContext<AuthContextData>(
 
 export const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
-    const token = localStorage.getItem('@Precato:token');
-    const user = localStorage.getItem('@Precato:user');
+    const token = localStorage.getItem('@Survey:token');
 
-    if (token || user) {
-      api.defaults.headers.authorization = `Bearer ${token}`;
+    if (token) {
+      api.defaults.headers['x-access-token'] = token;
 
-      return { token, user: JSON.parse(user) };
+      return { token };
     }
 
     return {} as AuthState;
   });
 
-  let refresh_token_new: string;
-
   const signIn = useCallback(async ({ email, password }) => {
-    const response = await api.post('/sessions', {
-      email,
-      password,
-    });
+    await api
+      .post('/login', {
+        email,
+        password,
+      })
+      .then(response => {
+        if (response.status === 200) {
+          const { accessToken } = response.data;
 
-    const { token, user, refresh_token } = response.data;
+          api.defaults.headers['x-access-token'] = accessToken;
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    refresh_token_new = refresh_token;
+          setData({ token: accessToken });
 
-    if (user.first_access) {
-      localStorage.setItem('@Precato:token', token);
-
-      localStorage.setItem('@Precato:user', JSON.stringify(user));
-
-      setData({ token, user });
-    }
-
-    api.defaults.headers.authorization = `Bearer ${token}`;
+          localStorage.setItem('@Survey:token', accessToken);
+        }
+      });
   }, []);
 
   const signOut = useCallback(() => {
-    localStorage.removeItem('@Precato:token');
-    localStorage.removeItem('@Precato:user');
+    localStorage.removeItem('@Survey:token');
 
     setData({} as AuthState);
   }, []);
 
-  const verifyTokenExpiration = useCallback(async () => {
-    const token = localStorage.getItem('@Precato:token');
-
-    if (token) {
-      const { exp } = decode(token) as JwtPayload;
-
-      if (Date.now() >= Number(exp) * 1000) {
-        try {
-          const response = await api.post('/sessions/refresh-token', {
-            token: refresh_token_new,
-          });
-
-          const { token: refresh_token } = response.data;
-
-          localStorage.removeItem('@Precato:token');
-          localStorage.setItem('@Precato:token', refresh_token);
-
-          api.defaults.headers.authorization = `Bearer ${refresh_token}`;
-
-          setData({ user: data.user, token: refresh_token });
-        } catch (err) {
-          signOut();
-        }
-      }
-    }
-  }, [data.user, refresh_token_new, signOut]);
-
   return (
-    <AuthContext.Provider
-      value={{ user: data.user, signIn, signOut, verifyTokenExpiration }}
-    >
+    <AuthContext.Provider value={{ isLogged: !!data.token, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
